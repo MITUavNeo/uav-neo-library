c57a614fffefedf11b6b09223d31b40baccce946
Chris Lai <chrisclai02@gmail.com>
2026-03-30 00:55:32 -0400

    fix conf path

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.relpath('library'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'library')))
 
 
 # -- Project information -----------------------------------------------------
